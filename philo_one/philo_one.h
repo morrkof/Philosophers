d5cf7345c26924f1346445f4d150342647caeb22
morrkof <morrkof@gmail.com>
2021-03-30 01:37:51 +0300

    initial

---
+++ philo_one/philo_one.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_one.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ppipes <ppipes@student.21-school.ru>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/26 15:12:00 by ppipes            #+#    #+#             */
+/*   Updated: 2021/01/26 15:14:23 by ppipes           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_ONE_H
+# define PHILO_ONE_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <string.h>
+
+#endif
